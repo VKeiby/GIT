9316645270a63dffe66575dc1cea2df5627b6d77
Vic.keiby <vic.keiby@techork.com>
2021-12-17 16:50:16 +0200

    multiSrv.bot.8.0

--- Kivork/server/pyBot.py
+++ Kivork/server/pyBot.py
@@ -1,5 +1,7 @@
+# This Python file uses the following encoding: utf-8
 import discord
 from discord.ext import commands
+# from discord import Activity, ActivityType
 
 PREFX = '.'
 client = commands.Bot(command_prefix=PREFX)
@@ -11,10 +13,39 @@ client.remove_command('help')
 # https://discordpy.readthedocs.io/en/stable/ :docs
 # https://discordpy.readthedocs.io/en/latest/api.html :docs
 # https://discordapp.com/oauth2/authorize?client_id=ID&scope=bot&permission=8 :connect
+'''
+
+# Bot about ######################
+client = discord.Client
+
+class OwnerCommands(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("OwnerCommands Is Ready")
+
+    @commands.command()
+    async def servers(self, ctx):
+        activeservers = client.guilds
+        for guild in activeservers:
+            await ctx.send(guild.name)
+            print(guild.name)
+
+def setup(client):
+    client.add_cog(OwnerCommands(client))
+
+##################################
+
+'''
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 @client.event
 async def on_ready():
-    print('BotConnected')
+    print('BotConnected as {0.user}'.format(client))
+
 
 # Clear msg
 @client.command(pass_context=True)
@@ -22,30 +53,26 @@ async def on_ready():
 async def clear(ctx, amount=100):  # .clear 100msg default
     await ctx.channel.purge(limit = amount)
 
+# \\\\\\\\\\\\\\\\\\\\\\\\\\\\  Who R U cmd   \\\\\\\\\\\\\\\\\\\\\\\\\\\
+@client.command()
+async def whoru(ctx, member: discord.Member):
+    await ctx.channel.purge( limit =1)
+    print(member.guild) # default Name
+    idUser = member.id
 
-# Clear cmd
-@client.command(pass_context=True)
-async def hello(ctx, amount=1):
-    await ctx.channel.purge(limit = amount+1)
-
-    author = ctx.message.author
-    await ctx.send(f'Hello {author.mention}.')
-
-@client.command(pass_context=True)
+@client.command()
 async def hi(ctx):
     author = ctx.message.author
     await ctx.send(f'Hello {author.mention}. I am a BOT for discord')
 
 # Kick
-@client.command(pass_context=True)
+@client.command()
 @commands.has_permissions(administrator = True)
 
 async def kick(ctx, member: discord.Member, *, reason = None):
     await ctx.channel.purge( limit =1)
-
     await member.kick( reason=reason)
     await ctx.send(f'User { member.mention} was kicked by admin')
-
 
 # Ban
 @client.command(pass_content= True)
@@ -53,10 +80,8 @@ async def kick(ctx, member: discord.Member, *, reason = None):
 
 async def ban(ctx, member: discord.Member, *, reason = None):
     await ctx.channel.purge( limit =1)
-
     await member.ban( reason=reason)
     await ctx.send(f'User { member.mention} was banned by admin for {reason}')
-
 
 # Unban
 @client.command(pass_content= True)
@@ -64,14 +89,11 @@ async def ban(ctx, member: discord.Member, *, reason = None):
 
 async def unban(ctx, *, member):
     await ctx.channel.purge( limit =1)
-
     banned_users = await ctx.guild.bans()
     for ban_entry in banned_users:
         user = ban_entry.user
-
         await ctx.guild.unban( user )
         await ctx.send (f'User { user.mention} was unbanned')
-
         return
 
 # HELP Command
@@ -91,22 +113,14 @@ async def help(ctx):
     await ctx.send( embed = emb)
 
 #__________________ Testing ________________________
-# You can also have a separate coroutine for your main server
-#@client.command()
-async def test():
-    test_guild_channel = client.get_channel(ID)
-    await test_guild_channel.send("Hello there!")
-
-@client.event
-async def on_message(message):
-    if client.user == message.author:
-        return
-    if message.guild.id == ID:
-        if message.content.lower() == ".hi":  # .lower() for case insensitivity
-            await message.channel.send('User was fucked by admin')
-        return
-    # Another condition for your main server + calling the other coroutine
-#----------------------------------------------------
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+@client.command()
+async def servers(self, ctx):
+    activeservers = client.guilds
+    for guild in activeservers:
+        await ctx.send(guild.name)
+        print(guild.name)
+# _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
 
 # Connect
 token = open('token.txt', 'r').readline()
